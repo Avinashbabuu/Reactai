ffa88587fd38019a3fa75b419020caf9d3d72261
Avinashbabuu <158088631+Avinashbabuu@users.noreply.github.com>
2025-05-22 16:29:23 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,15 @@
+
+bot_token = "7934617187:AAHicV3wznHZl37zWKoGXL5Z4txl9yo"
+SmmPanelApi = "9104d9d2b343c94gcf07007ac881c1f"
+SmmPanelApi2 = "fb8c1e3702hd9bbd10644254925fea9"
+viewsapi = "a2763b7besb1d206a682d419ad9da121"
+viewsapiurl = 'https:///api/v2'
+viewsserviceid = 3960
+admin_user_id = 6484788124
+required_channels = ['@Nepsxbot']
+payment_channel = "@botpaymentreq"
+logo_url = "https://ibb.co/bNwq1pL"
+welcome_bonus = 0
+ref_bonus = 0
+min_view = 10
+max_view = 50000
